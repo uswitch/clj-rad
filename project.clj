7f400c7a034c265508e8c4eebc1e857c3bfea120
Christian Blunden <christian.blunden@gmail.com>
2015-02-22 01:31:44 +0000

    add gpg credentials for clojars

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :resource-paths ["resources/surus-0.1.4.jar"]
+  :deploy-repositories [["clojars" {:creds :gpg}]]
   :dependencies [[org.clojure/clojure            "1.6.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]])
