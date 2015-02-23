6029f8ff0bc66892429b1594bed47c72af273aee
Christian Blunden <christian.blunden@gmail.com>
2015-02-23 11:36:17 +0000

    add github link for clojars source code

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
 (defproject clj-rad "0.1.0"
   :description "Clojure wrapper for Netflix Robust Anomaly Detection (RAD) library"
-  :url "http://github.com/uswitch/clj-rad"
+  :url "https://github.com/uswitch/clj-rad"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :resource-paths ["resources/surus-0.1.4.jar"]
+  :scm {:name "git"
+        :url "https://github.com/uswitch/clj-rad"}  
   :deploy-repositories [["clojars" {:creds :gpg}]]
   :dependencies [[org.clojure/clojure            "1.6.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]])
